bba2c6eeda07452cd7734c2d1c61790007e19f73
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-08 14:39:23 +0600

    smart tech store redux project already implement design by shadcn ui and implement  running also cart with redux toolkit

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from './features/cart/cartSlice';
+
+export const store = configureStore({
+    reducer: {
+        cart: counterReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
