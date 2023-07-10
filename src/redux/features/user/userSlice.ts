a497f189dcced743fada92b9fc034769007165da
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-10 23:34:28 +0600

    implement running firebase with create async thunk

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,52 @@
+
+import { auth } from "@/lib/firebase";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+interface IUserState {
+    user: {
+        email: string | null;
+    };
+    isLoading: boolean;
+    isError: boolean;
+    error: string | null;
+    
+}
+
+interface ICredential {
+    email: string;
+    password: string;
+}
+const initialState: IUserState ={
+    user: {
+        email: null,
+    },
+    isLoading: false,
+    isError: false,
+    error: null,
+}
+
+export const createUser = createAsyncThunk(
+    "user/createuser",
+    async ({email, password}: ICredential) => {
+        const data = await createUserWithEmailAndPassword(auth, email, password);
+        return data.user.email;
+    }
+)
+
+const userSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(createUser.fulfilled, () =>  {})
+        .addCase(createUser.rejected, () => {})
+    }
+})
+
+// export const {toggleState, setPriceRange} = userSlice.actions;
+
+export default userSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import counterReducer from './features/cart/cartSlice';
 import productReducer from './features/products/productSlice';
+import userReducer from './features/user/userSlice';
 import { api } from "./api/apiSlice";
 
 export const store = configureStore({
     reducer: {
         cart: counterReducer,
         product: productReducer,
+        user: userReducer,
         [api.reducerPath]: api.reducer 
     },
     middleware: (getDefaultMiddleware) =>
