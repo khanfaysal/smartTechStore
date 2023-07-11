877a22e38e05611c3bec6f833308371116dce21c
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-11 11:48:41 +0600

    user persistency added by firebase manage user, signup and login done

---
+++ src/lib/firebase.config.ts
@@ -0,0 +1,33 @@
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth"; // Modify this line
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app); // Modify this line
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUoakcz-oqvXUqPkt8LjcmHRzU4LJhxhs",
+  authDomain: "smart-tech-store.firebaseapp.com",
+  projectId: "smart-tech-store",
+  storageBucket: "smart-tech-store.appspot.com",
+  messagingSenderId: "525126862226",
+  appId: "1:525126862226:web:8d16e2169dab4a8bbe2d4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,7 +1,7 @@
 
-import { auth } from "@/lib/firebase";
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase.config";
+import {PayloadAction, createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 interface IUserState {
     user: {
@@ -34,19 +34,64 @@ export const createUser = createAsyncThunk(
     }
 )
 
+export const loginUser = createAsyncThunk(
+    "user/loginUser",
+    async ({email, password}: ICredential) => {
+        const data = await signInWithEmailAndPassword(auth, email, password);
+        return data.user.email;
+    }
+)
+
 const userSlice = createSlice({
     name: 'product',
     initialState,
-    reducers: {},
+    reducers: {
+        setUser: (state, action:PayloadAction<string>) => {
+            state.user.email = action.payload;
+        },
+        setLoading: (state, action:PayloadAction<boolean>) => {
+            state.isLoading = action.payload;
+        }
+    },
     extraReducers: (builder) => {
-        builder.addCase(createUser.pending, (state) => {
+        builder
+        .addCase(createUser.pending, (state) => {
             state.isLoading = true;
+            state.isError = false;
+            state.error = null;
         })
-        .addCase(createUser.fulfilled, () =>  {})
-        .addCase(createUser.rejected, () => {})
+        .addCase(createUser.fulfilled, (state, action) =>  {
+            state.user.email = action.payload;
+            state.isLoading = false;
+
+        })
+        .addCase(createUser.rejected, (state, action) => {
+            state.user.email = null;
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error.message!;
+            
+        })
+        .addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+            state.error = null;
+        })
+        .addCase(loginUser.fulfilled, (state, action) =>  {
+            state.user.email = action.payload;
+            state.isLoading = false;
+
+        })
+        .addCase(loginUser.rejected, (state, action) => {
+            state.user.email = null;
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error.message!;
+            
+        })
     }
 })
 
-// export const {toggleState, setPriceRange} = userSlice.actions;
+export const {setUser, setLoading} = userSlice.actions;
 
 export default userSlice.reducer;
